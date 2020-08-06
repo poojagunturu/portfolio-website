9bc93bcb2e52f6d2e1562f45f26dc39fb816cc55
Pooja Gunturu Venkata <vgxkm@mst.edu>
2020-08-05 19:36:30 -0500

    testing fix for firefox issue

--- default.js
+++ default.js
@@ -20,32 +20,32 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
 }
 
-if (document.body.getElementsByClassName("about-me-back")[0]) {
-  var i = 1;
-  window.addEventListener(
-    "scroll",
-    function () {
-      moveBackground();
-    },
-    false
-  );
-}
+// if (document.body.getElementsByClassName("about-me-back")[0]) {
+//   var i = 1;
+//   window.addEventListener(
+//     "scroll",
+//     function () {
+//       moveBackground();
+//     },
+//     false
+//   );
+// }
 
-function moveBackground() {
-  var speed = 90;
-  var back = document.body.getElementsByClassName("about-me-back")[0];
-  var str = window.getComputedStyle(back, null).backgroundPosition;
-  var res = str.split(" ");
-  var one = parseInt(res[0].slice(0, -2));
-  var two = parseInt(res[2].slice(0, -2));
-  var newPosOne = 10 * (speed / 100) + one;
-  var newPosTwo = two - 10 * (speed / 100);
-  if (newPosOne > 0 && newPosTwo < 0) {
-    newPosOne = 0;
-    newPosTwo = 0;
-  }
-  back.style.backgroundPosition = `${newPosOne}px 0px, ${newPosTwo}px 0px`;
-}
+// function moveBackground() {
+//   var speed = 90;
+//   var back = document.body.getElementsByClassName("about-me-back")[0];
+//   var str = window.getComputedStyle(back, null).backgroundPosition;
+//   var res = str.split(" ");
+//   var one = parseInt(res[0].slice(0, -2));
+//   var two = parseInt(res[2].slice(0, -2));
+//   var newPosOne = 10 * (speed / 100) + one;
+//   var newPosTwo = two - 10 * (speed / 100);
+//   if (newPosOne > 0 && newPosTwo < 0) {
+//     newPosOne = 0;
+//     newPosTwo = 0;
+//   }
+//   back.style.backgroundPosition = `${newPosOne}px 0px, ${newPosTwo}px 0px`;
+// }
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
